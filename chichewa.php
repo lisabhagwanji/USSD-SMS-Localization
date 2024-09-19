f8942ce0428d27c7cee1919e7846b0c7bfc56747
lisabhagwanji <139112939+lisabhagwanji@users.noreply.github.com>
2024-09-19 14:09:44 +0200

    Initial commit of PHP files

---
+++ chichewa.php
@@ -0,0 +1,57 @@
+<?php
+
+return[
+'sms_usr_reg_conf' =>'Takulandirani ku InspireLearn! Imbani 4274 kuti mukhazikitse ndi kutsatira za ma konferensi. Imbani *4274# kuti mudziwe zambiri',
+'sms_conf_schdl'=>'Kwakhazikitsidwa konferensi ya {course name} {tsiku} nthawi ya {time}. Imbani *4274# kuti mudziwe zambiri',
+'sms_conf-rmdr' => 'Chidziwitso! konferensi yanu ya {dzina la kalasi} iyamba mu mphindi 30',
+'sms_conf_upd' => 'Chidziwitso!Konferensi yanu yasintha ndipo ikhala {nthawi, tsiku}',
+'sms_conf_cncl' => 'Chidziwitso!Konferensi yanu ya {dzina la kalasi} yalephereka. Imbani 4274 kuti muone ma konferensi ena omwe alipo',
+'sms_imp_annc' => 'Tsopano mutha kugwiritsanso nchito pulatifomu ya InspireLearn poyimba *4274# ndi kutsatira ndondomeko',
+'sms_acc_dctv'=> 'Akaunti yanu yaimitsidwa. Kuti muyambenso kuigwiritsa ntchito imbani *4274# kapena yankhulani ndi aku InspireLearn pa {nambala}',
+'sms_bundl_prchs' =>'Mwagula bandulo ya {dzina la bundle} kwa {kutalika kwa nthawi}. Balansi yanu yatsopano ndi'
+
+/* USSD */
+'ussd_wlcm_new'=> 'Takulandirani ku InspireLearn. Popitiliza, mukutsimikizira kuti mwawerenga ndipo kuti mwagiwilzana ndi ndondomeko komanso malamulo athu. 1. Pitilizani 2. Tulukani'
+'ussd_access_tcs' => 'Mutha kupeza ndondomeko komanso malamulo athu pa inspirelearnmw.com kapena kusukulu yapafupi ndi inu'
+'ussd_entr_nm' => 'Lowetsani dzina lanu loyamba:'
+'ussd_usr_type' => 'Sankhani: 1. Ndine wophunzira 2. Ndine mphunzitsi 3. Zina'
+'ussd_usr_reg_conf' => ' Zonse zayenda bwino! Gwiritsani ntchito njira ya Maakaunti kuti musinthe ndikuonjezera zambiri.'
+'ussd_entr_crs_cd'=> 'Lembani nambala(code) ya kalasi yomwe mwapatsidwa'
+'ussd_invalid_cd' => 'Pepani, Nambalayo siya validi. chonde yesaniso nthawi ina mu njira ya Maakaunti.'
+'ussd_cls_conf' => 'Dziwani kuti mukulowa kalasi la Kamzimbi-Form4A monga mphunzitsi. Chonde tsimikizirani 1. Tsimikizirani 2. Tulukani'
+'ussd_try_again' => ' Chonde yesaniso nthawi ina mu njira ya Maakaunti.'
+'ussd_join_cls' => 'zonse zatheka! Sankhani pa akaunti kuti musinthe ndikuonjezera zambiri. '
+/*USSD WELCOME MENU FOR REGULAR USER */
+'ussd_wlcm' => 'Takulnadiranu InspireLearn! 1. Ma Bundulo 2. Zamma Kalasi/ Conferences 3. Akaunti'
+/*USSD BUNDLES MENU*/ 
+'ussd_bndls_menu'=> 'Ma Bandulo okhudaza InspireLearn 1. Kugula Bundle 2. Bandulo yotsala'
+'ussd_bndls' => 'Gulani ma Bandulo: 1. 27 mins, 24hrs @K639 2. 135 min, 7Days @K3199 3.'
+'ussd_bal' => 'Bandulo yanu yotsala ndi 27 mins ndipo ipanga expire pa 24/08/2024'
+'ussd_pay_conf'=> 'Chonde tsimikizirani kulipira 639.00 MWK ku InspireLearn po lowetsa numbala ya chinsisi (PIN) yanu kapena sindikizani 2. kuti musapitilize..'
+
+
+/*USSD CONFERENCES MENU */
+'ussd_cnfr_menu' => 'Ma Kalasi/ Conferences 1. Onani / Sinthani Nthawi ya ma kalasi 2. Khazikitsani nthawi   '
+'ussd_cnfr_available' => '1. Form4A-Biology lero @12:45 2.Form3-Biology mawa @13:40 '
+'ussd_cnfr_details' => 'Form4A-Biology @12:45 1. Kuti musinthe  2. Kuti musapitilize'
+'ussd_cnfr_cancel' => 'Kuchotsa maphunziro a Form4a-Biology omwe anakonzedwa pa 10 August @ 14:45. 1. Eya 2. Ayi'
+'ussd_slct_cls' => 'Sankhani Kalasi 1. Form4a-Biology 2. Form3-Biology'
+'ussd_cnfr_schdl' => 'Nthawi ya conference? 1. Lero 2. Mawa 3. Lowetsani tsiku (ddmm)'
+'ussd_entr_time' => 'Lowetsani nthawi (mmhh)'
+'ussd_schdl_time' => 'Khazikitsani Form4a-Biology pa 10  Aug @ 14:45 1. Tsimikizirani 2. Tulukani'
+/*USSD ACCOUNT SETTINGS MENU */
+/**Manage conferences */
+'ussd_acc_settings_menu' => 'Za Akaunti yanu 1. Dongosolo la ma Kalasi 2. Sinthani za pa profile yanu3. Dongosolo la zochitika pa pulatifomu 4. Kuyimitsa akaunti'
+'ussd_mng_cnfr' => 'Dongosolo la ma Kalasi 1. Lowani mu nKalasi 2. Onani ma Kalasi amene alipo'
+'ussd_ext_cnfr' => 'Kalasi yomwe mwasankha kutuluka 1. Form4a 2. Form3'
+'ussd_ext_conf' => 'Mwatsimikiza kutuluka kalasi la Kamzimbi-Form4a? 1. Eya 2. Ayi'
+/**Manage update profile */
+'ussd_ent_last_nm'=> 'Lowetsani dzina la bambo'
+'ussd_slct_gender'=>'Mwamuna-Mkazi? 1. Mwamuna 2. Mkazi'
+'ussd_entr_yob' => 'chaka chobadwa(YYYY)'
+'ussd_sp_needs' => 'Muli ndi ulumali uliwonse? 1. Eya 2. Ayi'
+/*platform settings */
+'ussd_plat_settings' => 'Sankhani Chiyankhulo 1. English 2. Chichewa'
+/*Deactivate */
+'ussd_dctv_acc' => 'Kuyimitsa akaunti 1. Kuti Mutsimikize kuyimitsa 2. Kuti musapitilize'
+];
---
+++ english.php
@@ -0,0 +1,56 @@
+<?php
+
+return[
+/* SMS*/
+'sms_usr_reg_conf' => 'Welcome to InspireLearn! Dial 4274 to create, join and catch up on conferences. For more dial *4274#',
+'sms_conf_schdl'=>'Your course name conference is set for 15:30 on Thur 12 Sept. Call 4274 to join or *4274# for more',
+'sms_conf-rmdr' => 'Reminder! Your {conference name} conference starts in {time left}',
+'sms_conf_upd' => 'Conference Update! {conference name} has been shifted to {time, day}',
+'sms_conf_cncl' => 'Conference Update! Your {conf name} conference has been canceled. Dial *4274# to explore available conferences',
+'sms_imp_annc' => 'Attention! InspireLearn is now accessible via USSD, dial *4274# to explore',
+'sms_acc_dctv'=> 'Sad to see you go. To reactivate your account dial *4274# or contact our team {number}',
+'sms_bundl_prchs' => 'You have recharged with {bundle name} valid for {duration}. Your new balance is {balances + validity}'
+
+/* USSD */
+'ussd_wlcm_new'=> 'Welcome to InspireLearn! By continuing, you confirm that you have read and accepted our terms and conditions. 1. Continue 2. Cancel'
+'ussd_access_tcs' => 'You can access our terms and conditions, at inspirelearnmw.com or a nearest school.'
+'ussd_entr_nm' => 'Enter your first name:'
+'ussd_usr_type' => 'Select an option: 1. Am a student 2. Am a teacher 3. Other'
+'ussd_usr_reg_conf' => ' You are all set! Use the Accounts option to update and add more information.'
+'ussd_entr_crs_cd'=> 'Enter given course code'
+'ussd_invalid_cd' => 'The code is invalid, please try again later in the account option.'
+'ussd_cls_conf' => 'You are joining Kamzimbi-Form4A as a teacher. Please confirm 1. Confirm 2. Cancel'
+'ussd_try_again' => ' Please try again later in the account option.'
+'ussd_join_cls' => 'You are set! use the accounts option to edit and add more info.'
+/*USSD WELCOME MENU FOR REGULAR USER */
+'ussd_wlcm' => 'Welcome to InspireLearn! 1. Bundles 2. Conferences 3. Account'
+/*USSD BUNDLES MENU*/ 
+'ussd_bndls_menu'=> 'Bundles: 1. Buy Bundle 2. Check Balance'
+'ussd_bndls' => 'Bundles: 1. 27 mins, 24hrs @K639 2. 135 min, 7 Days @K3199 3.'
+'ussd_bal' => 'Balance: You have 27 mins expiring on 24/08/2024'
+'ussd_pay_conf'=> 'Please confirm payment of  639.00MWK to InspireLearn. Enter PIN to confirm or Press Cancel to Reject.'
+/*USSD CONFERENCES MENU */
+'ussd_cnfr_menu' => 'Conferences: 1. View / Update Conference 2. Schedule Conference'
+'ussd_cnfr_available' => '1. Form4A-Biology today @12:45 2.Form3-Biology  tomorr @13:40 '
+'ussd_cnfr_details' => 'Form4A-Biology @12:45 1. Update Conference 2. Cancel Conference'
+'ussd_cnfr_cancel' => 'Canceling  Form4a-Biology on 10  Aug @ 14:45 1.Yes 2. No'
+'ussd_slct_cls' => 'Select Class 1. Form4a-Biology 2. Form3-Biology'
+'ussd_cnfr_schdl' => 'Schedule conference for? 1.Today 2. Tomorrow 3. Enter date(ddmm)'
+'ussd_entr_time' => 'Enter time as mmhh'
+'ussd_schdl_time' => 'Schedule Form4a-Biology for 10  Aug @ 14:45 1.Confirm Confernce 2. Cancel Conference'
+/*USSD ACCOUNT SETTINGS MENU */
+/**Manage conferences */
+'ussd_acc_settings_menu' => 'Account Settings: 1. Manage Conference 2. Update profile 3. Platform settings 4. Deactivate'
+'ussd_mng_cnfr' => 'Manage Conference 1. Join Conference 2. View Conferences'
+'ussd_ext_cnfr' => 'Select conference to exit 1. Form4a 2. Form3'
+'ussd_ext_conf' => 'Continue to exit Kamzimbi-Form4a? 1. Continue.  2. Cancel'
+/**Manage update profile */
+'ussd_ent_last_nm'=> 'Enter your last name'
+'ussd_slct_gender'=>'Select your gender 1. Male 2. Female'
+'ussd_entr_yob' => 'Enter your year of birth (YYYY)'
+'ussd_sp_needs' => 'Do you have any special needs? 1.Yes 2.No'
+/*platform settings */
+'ussd_plat_settings' => 'Select Language 1. English 2. Chichewa'
+/*Deactivate */
+'ussd_dctv_acc' => 'Deactivating account 1. Continue 2. Cancel'
+];
